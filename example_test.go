8b5126e208540fa1b81aeaa9a9c244fcbc67356a
Taras Bunyk <taras.bunyk@zalando.de>
2024-04-07 20:25:57 +0200

    feat: Implement NameSep option

--- example_test.go
+++ example_test.go
@@ -53,6 +53,27 @@ func ExampleLoad_nestedStruct() {
 }
 
 func ExampleLoad_nestedStructPrefixed() {
+	os.Setenv("DBHOST", "localhost")
+	os.Setenv("DBPORT", "5432")
+
+	var cfg struct {
+		DB struct {
+			Host string `env:"HOST"`
+			Port int    `env:"PORT"`
+		} `env:"DB"`
+	}
+	if err := env.Load(&cfg, nil); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(cfg.DB.Host)
+	fmt.Println(cfg.DB.Port)
+	// Output:
+	// localhost
+	// 5432
+}
+
+func ExampleLoad_nestedStructPrefixedWithSeparator() {
 	os.Setenv("DB_HOST", "localhost")
 	os.Setenv("DB_PORT", "5432")
 
@@ -60,9 +81,9 @@ func ExampleLoad_nestedStructPrefixed() {
 		DB struct {
 			Host string `env:"HOST"`
 			Port int    `env:"PORT"`
-		} `env:"DB_"`
+		} `env:"DB"`
 	}
-	if err := env.Load(&cfg, nil); err != nil {
+	if err := env.Load(&cfg, &env.Options{NameSep: "_"}); err != nil {
 		fmt.Println(err)
 	}
 
